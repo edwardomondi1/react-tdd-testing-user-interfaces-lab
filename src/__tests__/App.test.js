464651a03b7d3d9745ddc525d974355b238c4841
edwardomondi1 <edward.omondi1@student.moringaschool.com>
2025-10-30 18:11:46 +0300

    message

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,8 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Your code here/i);
+  expect(linkElement).toBeInTheDocument();
+});
